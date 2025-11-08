c359ae61534719cbeb7abe6d260e891f0afe3072
doseobujang <doseobujang@naver.com>
2025-11-08 09:32:40 +0900

    import ttt_v3 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Ttt_v1 from './tic-tac-toe/ttt_v1.jsx';
 import Ttt_v2 from './tic-tac-toe/ttt_v2.jsx';
+import Ttt_v3 from './tic-tac-toe/ttt_v3.jsx';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <Ttt_v1 />
       <br />
       <Ttt_v2 />
+      <br />
+      <Ttt_v3 />
     </>
   );
 }
